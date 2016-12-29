d5850f69480b36d36cbe1e307584b2786bf3b2bb
Joel Geiger <joel@sibilantsolutions.com>
2016-12-28 20:31:36 -0500

    Initial commit.

---
+++ src/main/java/com/sibilantsolutions/ibex/IbexApplication.java
@@ -0,0 +1,12 @@
+package com.sibilantsolutions.ibex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbexApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sibilantsolutions/ibex/IbexApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sibilantsolutions.ibex;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IbexApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
