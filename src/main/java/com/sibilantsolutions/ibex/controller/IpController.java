249433024a934885b62c9b1561b333e78ac4dfa6
Joel Geiger <joel@sibilantsolutions.com>
2017-01-14 20:50:19 -0500

    Echo back all the client request headers.

--- src/main/java/com/sibilantsolutions/ibex/controller/IpController.java
+++ src/main/java/com/sibilantsolutions/ibex/controller/IpController.java
@@ -1,11 +1,13 @@
 package com.sibilantsolutions.ibex.controller;
 
 import com.sibilantsolutions.ibex.dto.ClientAddressDto;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletRequest;
+import java.util.Map;
 
 /**
  * Created by jt on 12/28/16.
@@ -16,12 +18,13 @@ import javax.servlet.ServletRequest;
 public class IpController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public ClientAddressDto getClientAddress(ServletRequest servletRequest) {
+    public ClientAddressDto getClientAddress(ServletRequest servletRequest, @RequestHeader Map<String, String> requestHeaders) {
 //TODO: Account for being behind a reverse proxy.  Detect IP from header if present.
         ClientAddressDto clientAddressDto = new ClientAddressDto();
 
         clientAddressDto.clientAddress = servletRequest.getRemoteAddr();
         clientAddressDto.clientPort = servletRequest.getRemotePort();
+        clientAddressDto.requestHeaders.putAll(requestHeaders);
 
         return clientAddressDto;
     }
--- src/main/java/com/sibilantsolutions/ibex/dto/ClientAddressDto.java
+++ src/main/java/com/sibilantsolutions/ibex/dto/ClientAddressDto.java
@@ -1,5 +1,10 @@
 package com.sibilantsolutions.ibex.dto;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by jt on 12/28/16.
  */
@@ -7,5 +12,6 @@ public class ClientAddressDto {
 
     public String clientAddress;
     public int clientPort;
+    final public Map<String, String> requestHeaders = new HashMap<>();
 
 }
